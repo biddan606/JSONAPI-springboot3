c782ddc580cd9a176b2347fcd71b2eabfdad048f
biddan606 <biddan606@gmail.com>
2025-03-22 17:34:46 +0900

    스프링부트3.0 프로젝트 생성

---
+++ src/main/java/dev/biddan/jsonapiexample/JsonapiExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.biddan.jsonapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonapiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonapiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/biddan/jsonapiexample/JsonapiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.biddan.jsonapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonapiExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
